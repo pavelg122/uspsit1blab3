d266d05d283a6deea2bd1a949348fc8b4e9c30b7
SvilenUrumov <svilen.rosenov.urumov@gmail.com>
2025-03-05 10:27:43 +0200

    Branch 1

--- .idea/test.cs
+++ .idea/test.cs
@@ -2,6 +2,6 @@ private int test;
 test = 10;
 Console.WriteLine(test);
 
-Console.WriteLine("This is a test file");
+Console.WriteLine("This is a test branch");
 
 Console.WriteLine("This is the branch code");
